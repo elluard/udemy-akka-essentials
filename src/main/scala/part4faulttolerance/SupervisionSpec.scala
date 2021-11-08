b2154d1eb66f16c70cde53b8d037667a6507b590
elluard <elluard@me.com>
2021-11-08 14:10:49 +0900

    ch. 26 완료

--- src/main/scala/part4faulttolerance/SupervisionSpec.scala
+++ src/main/scala/part4faulttolerance/SupervisionSpec.scala
@@ -1,8 +1,8 @@
 package part4faulttolerance
 
 import akka.actor.SupervisorStrategy.{Escalate, Restart, Resume, Stop}
-import akka.actor.{Actor, ActorSystem, OneForOneStrategy, Props}
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.actor.{Actor, ActorRef, ActorSystem, AllForOneStrategy, OneForOneStrategy, Props, SupervisorStrategy, Terminated}
+import akka.testkit.{EventFilter, ImplicitSender, TestKit}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.wordspec.AnyWordSpecLike
 
@@ -15,16 +15,112 @@ with BeforeAndAfterAll {
   }
 
   import SupervisionSpec._
+
+  "A supervisor" should {
+    "resume its child in case of minor fault" in {
+      val supervisor = system.actorOf(Props[Supervisor])
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      child ! "I love Akka"
+      child ! Report
+      expectMsg(3)
+
+      child ! "Akka is awesome because I am learning to think in a whole new way"
+      child ! Report
+      expectMsg(3)
+    }
+
+    "restart its child in case of an empty sentence" in {
+      val supervisor = system.actorOf(Props[Supervisor])
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      child ! "I love Akka"
+      child ! Report
+      expectMsg(3)
+
+      child ! ""
+      child ! Report
+      expectMsg(0)
+    }
+
+    "terminate its child in case of a major error" in {
+      val supervisor = system.actorOf(Props[Supervisor])
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      watch(child)
+      child ! "akka is nice"
+      val terminatedMessage = expectMsgType[Terminated]
+      assert(terminatedMessage.actor == child)
+    }
+
+    "escalate an error when it doesn't know what to do" in {
+      val supervisor = system.actorOf(Props[Supervisor], "Supervisor")
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      watch(child)
+      child ! 43
+
+      val terminatedMessage = expectMsgType[Terminated]
+      assert(terminatedMessage.actor == child)
+    }
+  }
+
+  "A kiner supervisor" should {
+    "not kill children in case it's restarted or escalates failures" in {
+      val supervisor = system.actorOf(Props[NoDeathOnRestartSupervisor], "Supervisor")
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      child ! "Akka is cool"
+      child ! Report
+      expectMsg(3)
+
+      child ! 45
+      child ! Report
+      expectMsg(0)
+    }
+  }
+
+  "An all-for-one supervisor" should {
+    "apply the all-for-one strategy" in {
+      val supervisor = system.actorOf(Props[AllForOneSupervisor])
+      supervisor ! Props[FussyWordCounter]
+      val child = expectMsgType[ActorRef]
+
+      supervisor ! Props[FussyWordCounter]
+      val secondChild = expectMsgType[ActorRef]
+
+      secondChild ! "Testing supervision"
+      secondChild ! Report
+      expectMsg(2)
+
+      EventFilter[NullPointerException]() intercept {
+        child ! ""
+      }
+
+      Thread.sleep(500)
+      secondChild ! Report
+      expectMsg(0)
+    }
+  }
 }
 
 object SupervisionSpec {
   case object Report
 
   class Supervisor extends Actor {
-    override val supervisorStrategy = OneForOneStrategy() {
+
+    //OneForOneStrategy 는 Exception 이 발생한 액터에 대해서만 적용한다.
+    //AllForOneStrategy 는 child actor 중 한곳에서 exception 이 발생하면 모든 actor 에 적용한다.
+    override val supervisorStrategy : SupervisorStrategy = OneForOneStrategy() {
       case _:NullPointerException => Restart
       case _:IllegalArgumentException => Stop
       case _:RuntimeException => Resume
+      // Escalate 의 경우, 모든 child actor 들을 종료시킨 후, 에러를 상위단계로 전달한다.
       case _:Exception => Escalate
     }
 
@@ -32,6 +128,22 @@ object SupervisionSpec {
       case props: Props =>
         val childRef = context.actorOf(props)
         sender() ! childRef
+    }
+  }
+
+  class NoDeathOnRestartSupervisor extends Supervisor {
+    override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+      //아무것도 안함
+    }
+  }
+
+  class AllForOneSupervisor extends Supervisor {
+    override val supervisorStrategy = AllForOneStrategy() {
+      case _:NullPointerException => Restart
+      case _:IllegalArgumentException => Stop
+      case _:RuntimeException => Resume
+      // Escalate 의 경우, 모든 child actor 들을 종료시킨 후, 에러를 상위단계로 전달한다.
+      case _:Exception => Escalate
     }
   }
 
